c95315f25b492a9c2460effab03758ea9950a33d
AlanL543 <73874120+AlanL543@users.noreply.github.com>
2021-04-05 22:50:12 +0200

    Add files via upload

---
+++ Reseau_Social-main/Reseau_Social-main/Projet_Mme_Dumas/Pagelog.php
@@ -0,0 +1,27 @@
+<?php
+require_once "Index.php"
+protected $posts;
+protected $statuts;
+
+public function EnLigne() {
+ // Faire une liste li verte
+}
+
+public function HorsLigne() {
+
+}
+
+public function recupposts() {
+  
+}
+
+echo "Bonjour ".$prenom.$nom.", nous sommes heureux de vous revoir parmis nous ! "
+
+if (session_status($sessionid) == PHP_SESSION_ACTIVE)
+  EnLigne();
+else
+  HorsLigne();
+
+
+
+ ?>
---
+++ Reseau_Social-main/Reseau_Social-main/Projet_Mme_Dumas/sql.php
@@ -0,0 +1,6 @@
+<?php
+	$dbo = new PDO('mysql:host=localhost;dbname=projet', root, root);
+protected function nM() {
+	$reqnM = 'INSERT INTO tusers VALUES ($id++, $prenom, $nom, $photo)';
+} 
+?>

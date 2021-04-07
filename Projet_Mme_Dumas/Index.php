42550b47d333c84992d18094f984fb4ffcb8d264
AlanL543 <73874120+AlanL543@users.noreply.github.com>
2021-04-07 14:22:04 +0200

    Add files via upload

--- Projet_Mme_Dumas/Index.php
+++ Projet_Mme_Dumas/Index.php
@@ -1,50 +1,7 @@
 <?php
+require_once "Index.php";
 session_start();
 $dbo = new PDO('mysql:host=localhost;dbname=projet', root, root);
-class user {
-private $id = 0;
-private $prenom;
-private $nom;
-private $photo;
-public $sessionid = session_id();
-
-function __construct($id, $prenom, $nom, $photo){
-  $this->id = $id;
-  $this->prenom = $prenom;
-  $this->nom = $nom;
-  $this->$photo = $photo;
-}
-
-protected function get_id() {
-  return $this->id;
-}
-
-public function get_prenom() {
-  return $this->prenom;
-}
-
-public function get_nom() {
-  return $this->nom;
-}
-public function get_photo() {
-  return $this->photo;
-}
-
-protected function set_id($id) {
-  $this->id = $id;
-}
-
-public function set_prenom($prenom) {
-  $this->prenom = $prenom;
-}
-
-public function set_nom($nom) {
-  $this->nom = $nom;
-}
-
-public function set_photo($photo) {
-  $this->photo = $photo;
-}
 public function nouveauMembre() {
   $membre = new user ($this->id, $this->prenom, $this->nom, $this->photo);
 }
@@ -54,7 +11,6 @@ if $_POST["Enregistrer"]{
 }
 else
   <script type="text/javascript">
-  alert(Formulaire incomplet, merci de renseigner les informations manquantes afin de créer votre compte ! );
+  alert('Formulaire incomplet, merci de renseigner les informations manquantes afin de créer votre compte ! ');
   </script>
-}
- ?>
+?>

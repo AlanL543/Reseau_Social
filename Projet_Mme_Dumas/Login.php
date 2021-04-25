78a27eb772509a32cad0d0d1a491074013c54162
AlanL543 <73874120+AlanL543@users.noreply.github.com>
2021-04-25 11:33:50 +0200

    Add files via upload

---
+++ Projet_Mme_Dumas/Login.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require_once "CmysqlConnect.php";
+require_once "Login.html";
+public function verifmdp () {
+	if $_POST['login'] != $util->getlogin(); {
+		
+	}
+	$_POST['mdp']
+}
+if (isset($_POST['Connexion'])) 
+	verifmdp();
+?>
--- Projet_Mme_Dumas/listeUsers.php
+++ Projet_Mme_Dumas/listeUsers.php
@@ -2,4 +2,4 @@
 require_once "CmysqlConnect.php"
 session_start();
 $dbo = new PDO ('mysql:host = localhost;dbname=projet', root, root);
-$requete = 'SELECT * FROM tusers
+$requete = 'SELECT * FROM tusers';

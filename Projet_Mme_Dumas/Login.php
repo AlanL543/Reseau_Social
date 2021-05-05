8548728f8d4651469dcbc9bb2b257b32cd2e0b84
AlanL543 <73874120+AlanL543@users.noreply.github.com>
2021-05-05 14:25:02 +0200

    Add files via upload

--- Projet_Mme_Dumas/Login.php
+++ Projet_Mme_Dumas/Login.php
@@ -2,12 +2,28 @@
 session_start();
 require_once "CmysqlConnect.php";
 require_once "Login.html";
-public function verifmdp () {
+/*public function verifmdp () {
 	if $_POST['login'] != $util->getlogin(); {
 		
-	}
-	$_POST['mdp']
+	}*/
+if (isset($_POST['Connexion'])) {
+	$log=$_POST['login']
+	$mm=$_POST['mdp']
+
+$dbo = new PDO ( 'mysql: host = localhost; dbname = projet' , "root", "root");
+$requete='SELECT login, mdp FROM tusers WHERE login=$log AND mdp=$mm';
+$dbo->query($requete);
+$num_row = mysql_num_rows($requete);
+
+switch ($num_row) {
+	case '0':
+		echo "Mot de passe éroné, veuillez réessayer ! "
+		break;
+	
+	default:
+		echo "Bienvenue cher membre ! "
+		break;
 }
-if (isset($_POST['Connexion'])) 
-	verifmdp();
+}
+
 ?>
